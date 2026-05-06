1065fc0d513c8a45d724d657bf03d366b0efb326
Kylie Bemis <k.bemis@northeastern.edu>
2026-05-05 21:48:10 -0400

    add do_sum2() and do_sum2_grouped()

--- src/stats.h
+++ src/stats.h
@@ -4,8 +4,30 @@
 #include <thread>
 #include "prelude.h"
 
-//// Unary kernel
-//----------------
+//// Vector statistics
+//---------------------
+#define OP_ADD 1
+#define OP_SUB 2
+#define OP_MUL 3
+#define OP_DIV 4
+
+template<int Op>
+double do_binop(double x, double y);
+
+template<> inline
+double do_binop<OP_ADD>(double x, double y) { return x + y; }
+
+template<> inline
+double do_binop<OP_SUB>(double x, double y) { return x - y; }
+
+template<> inline
+double do_binop<OP_MUL>(double x, double y) { return x * y; }
+
+template<> inline
+double do_binop<OP_DIV>(double x, double y) { return x / y; }
+
+//// Vector statistics
+//---------------------
 
 template<typename T>
 double do_sum(
@@ -22,11 +44,41 @@ double do_sum(
 		double xi = static_cast<double>(x[i * stride]);
 		if ( isIncomparable(xi) )
 			continue;
-		if ( weights != nullptr )
-			xi *= weights[i];
 		if ( abs )
 			xi = std::fabs(xi);
-		sum += std::pow(xi, p);
+		if ( weights != nullptr )
+			sum += weights[i] * std::pow(xi, p);
+		else
+			sum += std::pow(xi, p);
+	}
+	return sum;
+}
+
+template<typename Tx, typename Ty, int Op>
+double do_sum2(
+	const Tx * x,
+	const Ty * y,
+	const size_t len,
+	const ptrdiff_t x_stride = 1,
+	const ptrdiff_t y_stride = 1,
+	const double * weights = nullptr,
+	const bool abs = false,
+	const double p = 1)
+{
+	double sum = 0;
+	for ( size_t i = 0; i < len; ++i )
+	{
+		double xi = static_cast<double>(x[i * x_stride]);
+		double yi = static_cast<double>(y[i * y_stride]);
+		if ( isIncomparable(xi) || isIncomparable(yi) )
+			continue;
+		double di = do_binop<Op>(xi, yi);
+		if ( abs )
+			di = std::fabs(di);
+		if ( weights != nullptr )
+			sum += weights[i] * std::pow(di, p);
+		else
+			sum += std::pow(di, p);
 	}
 	return sum;
 }
@@ -49,11 +101,43 @@ void do_sum_grouped(
 		double xi = static_cast<double>(x[i * stride]);
 		if ( isIncomparable(xi) )
 			continue;
-		if ( weights != nullptr )
-			xi *= weights[i];
 		if ( abs )
 			xi = std::fabs(xi);
-		out_sums[group[i]] += std::pow(xi, p);
+		if ( weights != nullptr )
+			out_sums[group[i]] += weights[i] * std::pow(xi, p);
+		else
+			out_sums[group[i]] += std::pow(xi, p);
+	}
+}
+
+template<typename Tx, typename Ty, int Op>
+void do_sum2_grouped(
+	const Tx * x,
+	const Ty * y,
+	const size_t len,
+	double * out_sums,
+	const int * group,
+	const size_t ngroups,
+	const ptrdiff_t x_stride = 1,
+	const ptrdiff_t y_stride = 1,
+	const double * weights = nullptr,
+	const bool abs = false,
+	const double p = 1)
+{
+	fill_buffer<double>(out_sums, ngroups);
+	for ( size_t i = 0; i < len; ++i )
+	{
+		double xi = static_cast<double>(x[i * x_stride]);
+		double yi = static_cast<double>(y[i * y_stride]);
+		if ( isIncomparable(xi) || isIncomparable(yi) )
+			continue;
+		double di = do_binop<Op>(xi, yi);
+		if ( abs )
+			di = std::fabs(di);
+		if ( weights != nullptr )
+			out_sums[group[i]] += weights[i] * std::pow(di, p);
+		else
+			out_sums[group[i]] += std::pow(di, p);
 	}
 }
 
